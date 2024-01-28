59e62b4ab273b049aa10e83fc8451947004e0bd8
Aleksandr Sundeev <navigator97@mail.ru>
2024-01-28 09:41:56 +0700

    add test_sorted_operations_data

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,12 @@
-from src.functions import operations_check
+from src.functions import operations_check, sorted_operations_data
 
 
 def test_operations_check():
     assert operations_check([]) == []
     assert operations_check(['id']) != []
+
+
+def test_sorted_operations_data():
+    assert sorted_operations_data([]) == []
+    # assert sorted_operations_data(['id']) != []
 
