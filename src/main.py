983623b495d3e2a63657b47926ec755465aa0029
Aleksandr Sundeev <navigator97@mail.ru>
2024-01-28 14:31:19 +0700

    changed main.py

--- src/main.py
+++ src/main.py
@@ -12,11 +12,13 @@ def main():
                 "date": operations_data[i]["date"],
                 "description": operations_data[i]["description"],
                 "from": operations_data[i]["from"],
+                "to": operations_data[i]["to"],
             }
         else:
             operation = {
                 "date": operations_data[i]["date"],
                 "description": operations_data[i]["description"],
+                "to": operations_data[i]["to"],
             }
 
         date_of_operation = operation["date"][:-16]
@@ -24,23 +26,15 @@ def main():
         date_of_operation = date_of_operation[2] + '.' + date_of_operation[1] + '.' + date_of_operation[0]
 
         print(date_of_operation, operation["description"])
-        from_operation = operation.get('from', '-') + ' -> '
-        # print(from_operation)
-        if from_operation[0] != '-':
-            from_operation_number = from_operation[-20:-1]
-            from_operation_number = from_operation_number[:4] + ' ' + from_operation_number[5:7] + '** ****' + from_operation_number[14:17]
-            print(from_operation[0:-21], from_operation_number)
-        else:
-            print(from_operation)
-        # if from_operation != '-':
-        #     from_operation1 = from_operation[-16:-1]
-        # else:
-        #     from_operation1 = from_operation
-        # print(from_operation)
+        from_operation = operation.get('from', '') + ' '
+        to_operation = operation["to"]
+
+        from_operation_number = from_operation[-16:-1]
+        if from_operation != ' ':
+            from_operation_number = from_operation_number[:4] + ' ' + from_operation_number[4:6] + '** **** ' + from_operation_number[11:16]
+            print(from_operation[0:-18], from_operation_number, '->', to_operation)
 
         print('')
-        # for el in operations_data:
-        #     print(el["id"])
 
 
 if __name__ == '__main__':
