e04936d6ca8c91b889b27e8f0605c3756cddebb8
Aleksandr Sundeev <navigator97@mail.ru>
2024-01-29 07:07:13 +0700

    add test_load_operations & fixed test_get_operation & add test_get_operation__error

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,5 @@
-from src.functions import operations_check, sorted_operations_data
+import pytest
+from src.functions import operations_check, sorted_operations_data, get_operation, load_operations
 
 
 def test_operations_check():
@@ -10,3 +11,24 @@ def test_sorted_operations_data():
     assert sorted_operations_data([]) == []
     # assert sorted_operations_data(['id']) != []
 
+
+def test_get_operation():
+    assert get_operation([
+        {"date": "2019-08-26T10:50:58.294041",
+         "from": "Maestro 1596837868705199",
+         "to": "Счет 64686473678894779589",
+         "description": "Перевод организации"}
+    ], 0) == {'date': '2019-08-26T10:50:58.294041',
+              'description': 'Перевод организации',
+              'from': 'Maestro 1596837868705199',
+              'to': 'Счет 64686473678894779589'}
+
+
+def test_get_operation__error():
+    with pytest.raises(Exception):
+        get_operation({})
+
+
+def test_load_operations():
+    with pytest.raises(Exception):
+        load_operations('')
