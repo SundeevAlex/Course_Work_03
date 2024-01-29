fda6fc1cc6363bd199a1717fba8f9c4a6bef568b
Aleksandr Sundeev <navigator97@mail.ru>
2024-01-29 19:01:31 +0700

    remake test_print_from_operation to test_print_from_or_to_operation

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,7 @@
 import pytest
 from src.functions import operations_check, sorted_operations_data, get_operation
 from src.functions import load_operations, split_string, print_date_and_description
-from src.functions import print_from_operation
+from src.functions import print_from_or_to_operation
 
 
 def test_operations_check():
@@ -37,9 +37,9 @@ def test_load_operations():
 
 
 def test_split_string():
-    assert split_string('Visa Classic 345345') == ['Visa Classic ', '345345']
-    assert split_string('MasterCard 1796816785869527') == ['MasterCard ', '1796816785869527']
-    assert split_string('Visa Classic card 555') == ['Visa Classic card ', '555']
+    assert split_string('Visa Classic 345345') == ['Visa Classic ', '345345 ']
+    assert split_string('MasterCard 1796816785869527') == ['MasterCard ', '1796816785869527 ']
+    assert split_string('Visa Classic card 555') == ['Visa Classic card ', '555 ']
 
 
 def test_print_date_and_description():
@@ -50,9 +50,8 @@ def test_print_date_and_description():
          'to': 'Счет 35158586384610753655'}) == ('07.12.2019', 'Перевод организации')
 
 
-def test_print_from_operation():
-    assert print_from_operation(
-        {'date': '2019-12-07T06:17:14.634890',
-         'description': 'Перевод организации',
-         'from': 'Visa Classic 2842878893689012',
-         'to': 'Счет 35158586384610753655'}) == '2842 87** **** 9012'
+def test_print_from_or_to_operation():
+    assert print_from_or_to_operation(
+        {'from': 'Visa Classic 2842878893689012'}, 'from', '->') == '2842 87** **** 9012'
+    assert print_from_or_to_operation(
+        {'to': 'Master 2842878893689015'}, 'to', '') == '**9015'
