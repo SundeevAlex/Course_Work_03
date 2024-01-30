232b60226bfabfdc8407cceec5088d3799eac674
Aleksandr Sundeev <navigator97@mail.ru>
2024-01-30 15:40:53 +0700

    Add test_key_sort & test_print_all & fixed test_get_operation

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,8 @@
 import pytest
 from src.functions import operations_check, sorted_operations_data, get_operation
 from src.functions import load_operations, split_string, print_date_and_description
-from src.functions import print_from_or_to_operation, print_operation_amount
+from src.functions import print_from_or_to_operation, print_operation_amount, key_sort
+from src.functions import print_all
 
 
 def test_operations_check():
@@ -31,6 +32,21 @@ def test_get_operation():
                 'amount': '41096.24',
                 'name': 'USD'
             })
+    assert (get_operation([
+        {'id': 863064926,
+         'state': 'EXECUTED',
+         'date': '2019-12-08T22:46:21.935582',
+         'operationAmount':
+             {'amount': '41096.24', 'currency': {'name': 'USD', 'code': 'USD'}},
+         'description': 'Открытие вклада',
+         'to': 'Счет 90424923579946435907'}
+    ], 0) == {
+                'date': '2019-12-08T22:46:21.935582',
+                'description': 'Открытие вклада',
+                'to': 'Счет 90424923579946435907',
+                'amount': '41096.24',
+                'name': 'USD'
+            })
 
 
 def test_get_operation__error():
@@ -38,7 +54,7 @@ def test_get_operation__error():
         get_operation({})
 
 
-def test_load_operations():
+def test_load_operations_err():
     with pytest.raises(Exception):
         load_operations('')
 
@@ -75,3 +91,16 @@ def test_print_operation_amount():
             'amount': '17000.50',
             'name': 'руб.'
         }) == '17000.50 руб.'
+
+
+def test_key_sort():
+    assert key_sort({'id': 179194306, 'date': '2019-05-19T12:51:49.023880'}) == '2019-05-19T12:51:49.023880'
+    assert key_sort({'id': 179194306, 'date': '2019-05-19T12:51:49.023880', 'description': 'Перевод организации'}) == '2019-05-19T12:51:49.023880'
+
+
+def test_print_all():
+    assert print_all(
+        {'date': '2019-12-03T04:27:03.427014', 'description': 'Перевод с карты на карту',
+         'from': 'MasterCard 1796816785869527', 'to': 'Visa Classic 7699855375169288', 'amount': '17628.50',
+         'name': 'USD'}
+    ) == '17628.50 USD'
