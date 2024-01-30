895751c416e73c65a8c5490f7ec10c7bbca3ef7d
Aleksandr Sundeev <navigator97@mail.ru>
2024-01-30 15:41:30 +0700

    Fixed main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,13 @@
-from functions import load_operations, operations_check, sorted_operations_data
-from functions import get_operation, print_date_and_description
-from functions import print_from_or_to_operation, print_operation_amount
+from functions import load_operations, get_operation, getting_operations_data,print_all
 
 
 def main():
     operations_data = load_operations('operations.json')
-    operations_data = operations_check(operations_data)
-    operations_data = sorted_operations_data(operations_data)
+    operations_data = getting_operations_data(operations_data)
 
     for i in range(3):
         operation = get_operation(operations_data, i)
-
-        print_date_and_description(operation)
-
-        print_from_or_to_operation(operation, "from", '-> ')
-
-        print_from_or_to_operation(operation, "to", '')
-
-        print_operation_amount(operation)
+        print_all(operation)
 
 
 if __name__ == '__main__':
